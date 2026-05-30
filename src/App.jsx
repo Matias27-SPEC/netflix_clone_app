c8de0fff01030a112e05849b0a21904c6e5b8758
Matias27-SPEC <somat262723@gmail.com>
2026-05-30 11:51:19 +0300

    Fix Netflix hover card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import Banner from './components/Banner/Banner'
 import DisplayRow from './components/DisplayRow/DisplayRow'
 import Footer from './components/Footer/Footer'
